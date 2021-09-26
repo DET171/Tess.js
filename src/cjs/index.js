dcd11961ebb9d279247450565a165e44857418ba
Dusty <mrgeek484@gmail.com>
2021-09-26 16:23:09 +0800

    update docs

--- src/cjs/index.js
+++ src/cjs/index.js
@@ -1,36 +1,36 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+	value: true,
 });
-exports["default"] = void 0;
+exports['default'] = void 0;
 
-var _isNil = _interopRequireDefault(require("./misc/isNil.js"));
+var _isNil = _interopRequireDefault(require('./misc/isNil.js'));
 
-var _arrayify = _interopRequireDefault(require("./misc/arrayify.js"));
+var _arrayify = _interopRequireDefault(require('./misc/arrayify.js'));
 
-var _atob = _interopRequireDefault(require("./base64/atob.js"));
+var _atob = _interopRequireDefault(require('./base64/atob.js'));
 
-var _btoa = _interopRequireDefault(require("./base64/btoa.js"));
+var _btoa = _interopRequireDefault(require('./base64/btoa.js'));
 
-var _ranBool = _interopRequireDefault(require("./misc/ranBool.js"));
+var _ranBool = _interopRequireDefault(require('./misc/ranBool.js'));
 
-var _ranInt = _interopRequireDefault(require("./misc/ranInt.js"));
+var _ranInt = _interopRequireDefault(require('./misc/ranInt.js'));
 
-var _ranFloat = _interopRequireDefault(require("./misc/ranFloat.js"));
+var _ranFloat = _interopRequireDefault(require('./misc/ranFloat.js'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 /**
 * @copyright MIT Licensed | Copyright © 2019-present Dusty
 */
 var Tess = {};
-Tess.isNil = _isNil["default"];
-Tess.atob = _atob["default"];
-Tess.btoa = _btoa["default"];
-Tess.arrayify = _arrayify["default"];
-Tess.ranBool = _ranBool["default"];
-Tess.ranFloat = _ranFloat["default"];
-Tess.ranInt = _ranInt["default"];
+Tess.isNil = _isNil['default'];
+Tess.atob = _atob['default'];
+Tess.btoa = _btoa['default'];
+Tess.arrayify = _arrayify['default'];
+Tess.ranBool = _ranBool['default'];
+Tess.ranFloat = _ranFloat['default'];
+Tess.ranInt = _ranInt['default'];
 var _default = Tess;
-exports["default"] = _default;
+exports['default'] = _default;
